d78024e5165812fbdf2976fc43fc196298375c8a
Michael Haeuslmann <haeuslmann@gmail.com>
2016-03-11 21:41:40 +0100

    Add chapel

---
+++ src/main/java/dominium/Cards/Chapel.java
@@ -0,0 +1,24 @@
+package dominium.Cards;
+
+import dominium.GameMaster;
+import dominium.Players.Player;
+
+public class Chapel extends Card implements ActionCard {
+    public Chapel() {
+        cost = 2;
+        text = "Trash up to 4 cards from your hand.";
+    }
+
+    @Override
+    public void resolve(GameMaster master) {
+        int maxCards = 4;
+        Player player = master.currentPlayer();
+        Card selectedCard = player.selectCard(player.handCards());
+        while (maxCards > 0
+                && selectedCard != null) {
+            --maxCards;
+            player.trashCardFromHand(selectedCard);
+            selectedCard = player.selectCard(player.handCards());
+        }
+    }
+}
---
+++ src/test/java/dominium/Cards/ChapelTest.java
@@ -0,0 +1,41 @@
+package dominium.Cards;
+
+import dominium.GameMaster;
+import dominium.Players.Player;
+import dominium.Players.RandomPlayer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ChapelTest {
+    @Test
+    public void currentPlayerCanTrashOnlyTwoCards() {
+        GameMaster mockMaster = mock(GameMaster.class);
+        Player player = new RandomPlayer("Test");
+        player.handCards().add(new Copper());
+        player.handCards().add(new Silver());
+        when(mockMaster.currentPlayer()).thenReturn(player);
+
+        Chapel chapel = new Chapel();
+        chapel.resolve(mockMaster);
+        assertEquals(0, player.handCards().size());
+    }
+
+    @Test
+    public void currentPlayerCanTrashUpToFourCards() {
+        GameMaster mockMaster = mock(GameMaster.class);
+        Player player = new RandomPlayer("Test");
+        player.handCards().add(new Copper());
+        player.handCards().add(new Silver());
+        player.handCards().add(new Silver());
+        player.handCards().add(new Silver());
+        player.handCards().add(new Silver());
+        when(mockMaster.currentPlayer()).thenReturn(player);
+
+        Chapel chapel = new Chapel();
+        chapel.resolve(mockMaster);
+        assertEquals(1, player.handCards().size());
+    }
+}
