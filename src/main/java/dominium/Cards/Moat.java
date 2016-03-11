0c5428a46f37795ca7fbd27b20969a1c3d2a0fc1
Michael Haeuslmann <haeuslmann@gmail.com>
2016-03-11 01:24:21 +0100

    Fix Moat text

--- src/main/java/dominium/Cards/Moat.java
+++ src/main/java/dominium/Cards/Moat.java
@@ -5,7 +5,8 @@ import dominium.GameMaster;
 public class Moat extends Card implements ActionCard {
     public Moat() {
         cost = 2;
-        text = "+2 Cards";
+        text = "+2 Cards, When another player plays an Attack card, you may reveal this from your hand."
+                + " If you do, you are unaffected by that Attack.";
     }
 
     @Override
