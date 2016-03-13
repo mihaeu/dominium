290eba7f3b01995c8d828b6f7f8b8b4fe0d36f82
Michael Haeuslmann <haeuslmann@gmail.com>
2016-03-13 22:35:11 +0100

    Add predicate filter to KingdomCards

--- src/main/java/dominium/Cards/Mine.java
+++ src/main/java/dominium/Cards/Mine.java
@@ -4,9 +4,6 @@ import dominium.CardStack;
 import dominium.GameMaster;
 import dominium.Players.Player;
 
-import java.util.Stack;
-import java.util.stream.Collectors;
-
 public class Mine extends Card implements ActionCard {
     public Mine() {
         cost = 5;
@@ -18,22 +15,19 @@ public class Mine extends Card implements ActionCard {
     @Override
     public void resolve(GameMaster master) {
         Player player = master.currentPlayer();
-        CardStack treasureCardsOnHand = player.handCards().stream()
-                .filter(card -> card instanceof TreasureCard)
-                .collect(Collectors.toCollection(CardStack::new));
-        Card selectedCardForTrashing = player.selectCard(treasureCardsOnHand);
+        Card selectedCardForTrashing = player.selectCard(
+                master.currentPlayer().handCards().filterCards(CardType.Treasure)
+        );
         if (selectedCardForTrashing == null) {
             return;
         }
         player.trashCardFromHand(selectedCardForTrashing);
 
         int maxCost = selectedCardForTrashing.getCost() + 3;
-        CardStack cardsToChooseFrom = master.kingdomCards().values().stream()
-                .filter(stack -> stack.size() > 0)
-                .filter(stack -> stack.peek().cost <= maxCost)
-                .filter(stack -> stack.peek() instanceof TreasureCard)
-                .map(Stack::peek)
-                .collect(Collectors.toCollection(CardStack::new));
+        CardStack cardsToChooseFrom = master.kingdomCards()
+                .mapToCards(card -> master.kingdomCards().get(card).size() > 0)
+                .filterCards(card -> card.cost <= maxCost)
+                .filterCards(card -> card.getTypes().contains(CardType.Treasure));
 
         Card selectedCard = player.selectCard(cardsToChooseFrom);
         if (selectedCard == null) {
--- src/main/java/dominium/KingdomCardMap.java
+++ src/main/java/dominium/KingdomCardMap.java
@@ -3,6 +3,17 @@ package dominium;
 import dominium.Cards.Card;
 
 import java.util.HashMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class KingdomCardMap extends HashMap<Card, CardStack> {
+    /**
+     * @param predicate Filter which is applied to all keys
+     * @return CardStack filled with the filtered cards
+     */
+    public CardStack mapToCards(Predicate<Card> predicate) {
+        return keySet().stream()
+                .filter(predicate)
+                .collect(Collectors.toCollection(CardStack::new));
+    }
 }
